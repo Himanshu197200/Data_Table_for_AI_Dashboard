4241f9b13df5c81019a7aa21c47772bc69d5bfd8
Himanshu Mishra <himanshumishra4926@gmail.com>
2026-02-28 14:44:38 +0530

    add index file for exporting Table components and utilities

---
+++ src/components/Table/index.js
@@ -0,0 +1,5 @@
+export { default as Table } from './Table';
+export { default as TableHeader } from './TableHeader';
+export { default as TableRow } from './TableRow';
+export { default as Pagination } from './Pagination';
+export { default as SearchBar } from './SearchBar';
